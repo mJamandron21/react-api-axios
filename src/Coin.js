eca502a53f055367cc530325d78d0dd4fa9a8ee8
Mark Kenneth Jamandron <mjamandron@gmail.com>
2022-01-22 09:49:35 +0800

    minor fix

--- src/Coin.js
+++ src/Coin.js
@@ -42,7 +42,7 @@ const Coin = ({
           )}
 
           <p className='coin-marketcap'>
-            Mkt Cap: ${marketcap.toLocaleString()}
+            ${marketcap.toLocaleString()}
           </p>
         </div>
       </div>
